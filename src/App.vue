4729f278f69adcb147d01dd4654b7444071663dd
hahahh <2011358693@qq.com>
2022-07-25 17:18:06 +0800

    集成pinia

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div>Vite + Vue</div>
+  <h3>当前计数: {{ count }}</h3>
+  <button @click="increment">+1</button>
+  <button @click="decrement">-1</button>
 </template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useCount } from '@/store'
+
+const countStore = useCount()
+
+const { count } = storeToRefs(countStore)
+const { increment, decrement } = useCount()
+</script>
 
 <style scoped></style>
---
+++ src/store/count/index.ts
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+
+import { ref } from 'vue'
+
+export const useCount = defineStore('count', () => {
+  const count = ref(0)
+
+  const increment = () => count.value++
+
+  const decrement = () => count.value--
+
+  return {
+    count,
+    increment,
+    decrement
+  }
+})
