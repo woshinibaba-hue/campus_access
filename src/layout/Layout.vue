c511cfd3b9046f3189a02e58e914a0530143d45b
hahahh <2011358693@qq.com>
2022-11-19 15:27:44 +0800

    '自动引入icons所有图标'

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,6 +1,19 @@
 <template>
   <el-container>
-    <el-aside width="200px">Aside</el-aside>
+    <el-aside width="200px">
+      <el-menu default-active="1" class="el-menu-vertical-demo">
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><IconEpPlatform /></el-icon>
+            <span>系统管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          </el-menu-item-group>
+        </el-sub-menu>
+      </el-menu>
+    </el-aside>
     <el-container>
       <el-header>Header</el-header>
       <el-main>Main</el-main>
@@ -12,16 +25,8 @@
 <script setup lang="ts"></script>
 
 <style scoped lang="less">
-.el-header {
-  background-color: skyblue;
-}
-
 .el-main {
   height: calc(100vh - 120px);
   background-color: #f5f5f5;
-}
-
-.el-footer {
-  background-color: skyblue;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
@@ -26,7 +29,23 @@ export default defineConfig({
       resolvers: ElementPlusResolver()
     }),
     Components({
-      resolvers: ElementPlusResolver()
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'icon' // 自动引入的Icon组件统一前缀，默认为 i，设置false为不需要前缀
+          // {prefix}-{collection}-{icon} 使用组件解析器时，您必须遵循名称转换才能正确推断图标。
+          // alias: { park: 'icon-park' } 集合的别名
+          // enabledCollections: ['ep', 'entypo'] // 这是可选的，默认启用 Iconify 支持的所有集合['mdi']
+        })
+      ]
+    }),
+    Icons({
+      // scale: 1, // 缩放
+      compiler: 'vue3', // 编译方式
+      // defaultClass: '', // 默认类名
+      // defaultStyle: '', // 默认样式
+      autoInstall: true // 自动下载
+      // jsx: 'react' // jsx支持
     })
   ],
   // 配置别名
